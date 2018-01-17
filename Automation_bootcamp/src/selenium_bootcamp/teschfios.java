bd170989f6b33eef7ca3f2b579f4ea4a27fa1969
smi_s <smi_s@DESKTOP-VVI0H3R>
2018-01-17 12:57:11 -0600

    test

---
+++ Automation_bootcamp/src/selenium_bootcamp/teschfios.java
@@ -0,0 +1,21 @@
+package selenium_bootcamp;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class teschfios {
+	@Test
+	
+	public void AddExpense() {
+	
+		System.setProperty("webdriver.chrome.driver", "C:\\auto\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.hotmail.com");
+	
+	
+	
+	
+	
+	}			
+}
